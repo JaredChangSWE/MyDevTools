7c14f28787500ff2b76d5cbb1a36ae782aa086d4
JaredChang <qifen.pocket@gmail.com>
2025-02-13 10:18:38 +0800

    update

--- Mac/zsh/init.zsh
+++ Mac/zsh/init.zsh
@@ -4,5 +4,13 @@ PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
 PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
+ANDROID_HOME=/Users/jaredchang/Library/Android/sdk
+PATH=$PATH:$ANDROID_HOME/emulator
+PATH=$PATH:$ANDROID_HOME/platform-tools
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
 printf '\eP$f{"hook": "SourcedRcFileForWarp", "value": { "shell": "zsh"}}\x9c'
 
